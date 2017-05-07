ea6a119516cadf8723a4d810ea402c7b1ff02a28
Ori Hoch <ori@uumpa.com>
2017-05-07 08:47:59 +0300

    set proper version in setup.py - to support both versioned and unversioned package installation

--- setup.py
+++ setup.py
@@ -4,9 +4,16 @@ try:
 except ImportError:
     from distutils.core import setup
 
+if os.path.exists("VERSION.txt"):
+    # this file can be written by CI tools (e.g. Travis)
+    with open("VERSION.txt") as version_file:
+        version = version_file.read().strip().strip("v")
+else:
+    version = str(time.time())
+    
 setup(
     name='python-hebrew-numbers',
-    version='0.1.0',
+    version=version,
     author='Ori Hoch',
     author_email='ori@uumpa.com',
     description='conversion of hebrew numbers (Gimatria)',
