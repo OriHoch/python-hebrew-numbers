58201f24d17a25d6f962ee10a4439b65598b6ee9
Aaron Christianson <ninjaaron@gmail.com>
2017-06-13 10:00:41 +0200

    Fixes #6. added hebrew-special numbers as a submodule.

--- hebrew_numbers/tests.py
+++ hebrew_numbers/tests.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import unittest
-from . import gematria_to_int
+from . import gematria_to_int, int_to_gematria
 
 
-class Test(unittest.TestCase):
+class TestGematriaToInt(unittest.TestCase):
 
     def test(self):
         for heb, num in (
@@ -16,8 +16,29 @@ class Test(unittest.TestCase):
         ):
             self.assertEqual(gematria_to_int(heb), num)
 
-    def test(self):
+    def test_thousands(self):
         self.assertEqual(gematria_to_int(u'ה\'תשע"ז'), 5777)
+
+
+class TestIntToGematria(unittest.TestCase):
+
+    def test_with_gershayim(self):
+        for heb, num in (
+            (u'ב׳', 2),
+            (u'קצ״ח', 198),
+            (u'רח״צ', 298),
+            (u'ט״ז', 16),
+        ):
+            self.assertEqual(int_to_gematria(num), heb)
+
+    def test_no_gershayim(self):
+        for heb, num in (
+            (u'ב', 2),
+            (u'קצח', 198),
+            (u'רחצ', 298),
+            (u'טז', 16),
+        ):
+            self.assertEqual(int_to_gematria(num, gershayim=False), heb)
 
 
 if __name__ == '__main__':
